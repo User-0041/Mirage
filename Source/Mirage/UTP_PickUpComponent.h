6fbc77ce8aceaa6978ec2fc92f6cffb8a21c080b
User-0041 <qhmed0826@gmail.com>
2023-03-19 21:40:29 +0100

    push

---
+++ Source/Mirage/UTP_PickUpComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "UTP_PickUpComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIRAGE_API UUTP_PickUpComponent : public USphereComponent
+{
+	GENERATED_BODY()
+	
+};
